78ac4a402760e3d3ca99f69b255567820a1b9070
khaihoan-se <khaihoan.softwareengineer@gmail.com>
2022-06-04 21:55:55 +0700

    Register

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -12,7 +12,7 @@ const transition = { duration: 0.4, ease: [ 0.43, 0.13, 0.23, 0.96 ]}
 
 const RegisterPage: NextPage = () => {
     return (
-        <div className="bg--gradient relative text-center text-black">
+        <div className="bg--gradient relative text-black">
             {/* Button Back Home */}
             <motion.div
                 initial={{left: '3rem', opacity: 0 }}
@@ -37,20 +37,44 @@ const RegisterPage: NextPage = () => {
                 transition={transition}
                 className="md:h-[65vh] h-[100vh] w-full m-0 p-0 flex items-center justify-center"
             >
-                <div className="md:w-[768px] md:h-[60vh] w-full h-full bg-white z-50 md:rounded-md flex items-center overflow-hidden">
+                <div className="md:w-[768px] md:h-[60vh] w-full h-full bg-white z-50 md:rounded-md flex items-center overflow-hidden relative">
                     <div className="md:block hidden w-[45%] h-full relative border-r border-slate-400 cursor-pointer">
                         <Image src="/bg-register-02.jpeg" layout="fill" objectFit="contain" alt="logo" className="w-full h-full scale-105 hover:scale-110 duration-300 ease-in-out" />
                     </div>
-                    <div className="md:w-[55%] w-full h-full px-6 py-4">
-                        <div className="pr-8 leading-none">
-                            <h2 className="text-3xl font-semibold font-dancing">Login AniPhoto</h2>
-                            <span className="text-sm">Don't waste time knocking down a wall hoping to turn it into a door.</span>
+                    <div className="md:w-[55%] w-full h-full px-6 md:py-4 py-6 flex flex-col items-center md:items-start">
+                        <div className="pr-0 md:pr-8 leading-none">
+                            <h2 className="text-3xl font-semibold font-dancing">Register AniPhoto</h2>
+                            <span className="text-sm">Welcome to Aniphoto</span>
                         </div> 
-                        <div className="mt-4">
-                            <Input InputFor="name" label="Gmail" />
+                        <div className="mt-4 w-full">
+                            <Input InputFor="name" label="Full name" />
                         </div>
-                        <div className="mt-6">
+                        <div className="mt-4 w-full">
+                            <Input InputFor="gmail" label="Gmail" />
+                        </div>
+                        <div className="mt-4 w-full">
                             <Input InputFor="password" label="Password" />
+                        </div>
+                        <div className="mt-4 w-full">
+                            <Input InputFor="cfpassword" label="Enter the password" />
+                        </div>
+                        <div className="md:w-80 w-full mt-2">
+                            <p className="text-sm text-gray-700">Do you already have an account
+                                <Link href='/login'>
+                                    <span className="text-[#0652DD] ml-[3px] cursor-pointer underline">login</span>
+                                </Link>
+                            </p>
+                        </div>
+                        <div className="mt-4 w-full">
+                            <Button 
+                                className="button__aff relative md:w-80 w-full border-[1px] border-black rounded-md outline-none p-2 text-sm"
+                            >
+                                Login
+                            </Button>
+                        </div>
+
+                        <div className="md:w-80 w-full text-center absolute bottom-2">
+                            <span className="text-sm text-gray-700 text-center">© AniPhoto.vercel.app</span>
                         </div>
                     </div>
                 </div>
